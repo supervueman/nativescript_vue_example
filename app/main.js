9f6b13fdb42d36d8d5907fb55ac56cc767f899ec
supervueman <supervueman@gmail.com>
2019-06-22 14:18:19 +0400

    Add bottom tabs

--- app/main.js
+++ app/main.js
@@ -1,17 +1,25 @@
 import Vue from 'nativescript-vue'
-import App from './components/App'
+import App from './App'
 import store from './store';
+import apolloProvider from './plugins/apolloProvider';
 
 import VueDevtools from 'nativescript-vue-devtools'
 
 if (TNS_ENV !== 'production') {
-  Vue.use(VueDevtools)
+  Vue.use(VueDevtools, {
+    host: '192.168.1.42'
+  })
 }
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production')
 
+Vue.registerElement('Gradient', () => require('nativescript-gradient').Gradient)
+
+Vue.registerElement('Carousel', () => require('nativescript-carousel').Carousel)
+Vue.registerElement('CarouselItem', () => require('nativescript-carousel').CarouselItem)
 
 new Vue({
   store,
+  apolloProvider,
   render: h => h('frame', [h(App)])
 }).$start()
